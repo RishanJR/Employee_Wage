2e2fc241a4fc8ef7bab7c1112b4966af2295b1f7
RishanJR <rishanrajkumar@yahoo.com>
2020-09-21 21:09:03 +0400

    Adding part time and computing wages

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,18 +1,26 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo Welcome to Employee Wage Computation
 
 #Constants
 WAGE_PER_HOUR=20
-FULL_DAY=8
+FULL_TIME=8
+PART_TIME=4
 
 #Attendance check
-ATTENDANCE=$(( RANDOM%2 ))
-if [ $ATTENDANCE == 0 ]
+ATTENDANCE=$(( RANDOM%3 ))
+if [ $ATTENDANCE == 0 ]		##check for full time
 then
-	echo The employee is present
-	DAILY_WAGE=$(( $FULL_DAY*$WAGE_PER_HOUR ))
+	echo The employee is present and worked full time
+	DAILY_WAGE=$(( $FULL_TIME*$WAGE_PER_HOUR ))
 	echo The Daily Employee wage is $DAILY_WAGE
+
+elif [ $ATTENDANCE == 1 ]	##check for part time
+then
+	echo The employee is present and worked half time
+	DAILY_WAGE=$(( $PART_TIME*$WAGE_PER_HOUR ))
+	echo The Daily Employee wage is $DAILY_WAGE
+
 else
 	echo The employee is absent
 	echo The Daily Employee wage is 0

175609add97ff0942eca9976565cb7e333271fdf
RishanJR <rishanrajkumar@yahoo.com>
2020-09-21 21:20:56 +0400

    Solving using Switch Case

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -9,19 +9,23 @@ PART_TIME=4
 
 #Attendance check
 ATTENDANCE=$(( RANDOM%3 ))
-if [ $ATTENDANCE == 0 ]		##check for full time
-then
-	echo The employee is present and worked full time
-	DAILY_WAGE=$(( $FULL_TIME*$WAGE_PER_HOUR ))
-	echo The Daily Employee wage is $DAILY_WAGE
 
-elif [ $ATTENDANCE == 1 ]	##check for part time
-then
-	echo The employee is present and worked half time
-	DAILY_WAGE=$(( $PART_TIME*$WAGE_PER_HOUR ))
-	echo The Daily Employee wage is $DAILY_WAGE
-
-else
-	echo The employee is absent
-	echo The Daily Employee wage is 0
-fi
+case $ATTENDANCE in
+	0)
+		echo The employee is present and worked full time
+		DAILY_WAGE=$(( $FULL_TIME*$WAGE_PER_HOUR ))
+		echo The Daily Employee wage is $DAILY_WAGE
+		;;
+	1)
+		echo The employee is present and worked half time
+		DAILY_WAGE=$(( $PART_TIME*$WAGE_PER_HOUR ))
+		echo The Daily Employee wage is $DAILY_WAGE
+		;;
+	2)
+		echo The employee is absent
+		echo The Daily Employee wage is 0
+		;;
+	*)
+		echo Unprecedented situation occured
+		;;
+esac

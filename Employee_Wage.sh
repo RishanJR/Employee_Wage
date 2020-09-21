d8aa47d0dafd561dfb4d4a3f583da75885aad8f4
RishanJR <rishanrajkumar@yahoo.com>
2020-09-21 18:32:10 +0400

    Attendance check

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo Welcome to Employee Wage Computation
+
+#Attendance check
+
+ATTENDANCE=$(( RANDOM%2 ))
+if [ $ATTENDANCE == 0 ]
+then
+	echo The employee is present
+else
+	echo The employee is absent
+fi

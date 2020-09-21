43e6a81d91f0652b4d63319544bf2b662c1f4997
RishanJR <rishanrajkumar@yahoo.com>
2020-09-21 18:55:51 +0400

    Daily Employee Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -2,12 +2,18 @@
 
 echo Welcome to Employee Wage Computation
 
-#Attendance check
+#Constants
+WAGE_PER_HOUR=20
+FULL_DAY=8
 
+#Attendance check
 ATTENDANCE=$(( RANDOM%2 ))
 if [ $ATTENDANCE == 0 ]
 then
 	echo The employee is present
+	DAILY_WAGE=$(( $FULL_DAY*$WAGE_PER_HOUR ))
+	echo The Daily Employee wage is $DAILY_WAGE
 else
 	echo The employee is absent
+	echo The Daily Employee wage is 0
 fi
